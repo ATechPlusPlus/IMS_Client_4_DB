22bc90c0446a7f843739f74bf5416cd780106fc8
AAMIR <khan89aamir@gmail.com>
2021-01-06 03:13:08 +0530

    Add project files.

---
+++ IMS_Client_4_DB/Tables/UserManagement.sql
@@ -0,0 +1,24 @@
+
+CREATE TABLE [dbo].[UserManagement](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[UserName] [nvarchar](50) NULL,
+	[Password] [nvarchar](max) NULL,
+	[EmailID] VARCHAR(50) NULL,
+    [EmployeeID] INT NULL, 
+	[IsAdmin] [bit] NULL,
+	[ActiveStatus] [bit] DEFAULT 1,
+	[SecurityQuestion] [nvarchar](50) NULL,
+	[Answer] [nvarchar](100) NULL,
+	[CreatedBy] [int] NULL CONSTRAINT [DF_UserManagement_CreatedBy]  DEFAULT(0),
+	[CreatedOn] [datetime] NULL CONSTRAINT [DF_UserManagement_CreatedOn]  DEFAULT (getdate()),
+	[UpdatedBy] [int] NULL,
+	[UpdatedOn] [datetime] NULL, 
+    CONSTRAINT [PK_UserManagement] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+
